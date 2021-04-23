0db8794b7067171b503b63b0a51b326eacb35b43
Alina Krivosheenkova <alina88lis@gmail.com>
2021-04-23 15:13:20 +0300

    rfm.sql updated

--- 4_typical_analysis_of_market_activity/rfm.sql
+++ 4_typical_analysis_of_market_activity/rfm.sql
@@ -18,6 +18,7 @@ SET @M4 := 10000;
 SET @M5 := 50000;
 
 SET @last_date := (SELECT MAX(`o_date`) FROM `orders_20190822`);
+SET @overall_sum := (SELECT SUM(`price`) FROM `orders_20190822` WHERE `price` > 0);
 
 WITH
 	  `R` as (SELECT  `user_id`, 
@@ -39,14 +40,17 @@ WITH
 	  	      WHERE `price` > 0 
 	  	      GROUP BY `user_id`),
 	  
-	  `RF` as (SELECT `R`.`user_id`, 
+	  `RFM` as (SELECT `R`.`user_id`, 
 	  				  `R`.`days` as `R`, 
 	  				  `F`.`cnt` as `F`, 
-	  				  CASE  WHEN `R`.`days` > @R5 THEN 5
-	            			WHEN  `R`.`days` BETWEEN @R4 AND @R5 THEN 4
-	            			WHEN `R`.`days` BETWEEN @R3 and @R4 THEN 3
-	            			WHEN `R`.`days` BETWEEN @R3 AND @R1 THEN 2
-	      					ELSE 1 
+	  				  `M`.`sum_price` as `M`,
+	  				  CASE  
+	  				  		WHEN `R`.`days` < @R1 THEN 1
+	  				  	    WHEN `R`.`days` BETWEEN @R3 AND @R1 THEN 2
+	  				  	    WHEN `R`.`days` BETWEEN @R3 and @R4 THEN 3
+	  				  	    WHEN  `R`.`days` BETWEEN @R4 AND @R5 THEN 4
+	  				  		WHEN `R`.`days` > @R5 THEN 5
+	      					ELSE 0
 	      					END as 
 	      						`R1`,
 
@@ -54,7 +58,8 @@ WITH
 	            			WHEN  `F`.`cnt` BETWEEN @F2 AND @F3 THEN 2
 	            			WHEN `F`.`cnt` BETWEEN @F3 AND @F4 THEN 3
 	            			WHEN `F`.`cnt` BETWEEN @F4 AND @F5 THEN 4
-	      					ELSE 5
+	      					WHEN `F`.`cnt` > @F5 THEN 5
+	      					ELSE 0 
 	      			  END as `F1`, 
 
 	      			  CASE WHEN `M`.`sum_price` BETWEEN @M1 AND @M2 THEN 1
@@ -73,40 +78,69 @@ WITH
 	    	   INNER JOIN `M`
 	    	   ON `F`.`user_id` = `M`.`user_id`),
 
-	  `USERGROUPS` AS (SELECT `RF`.`user_id`,
-	  						  `RF`.`R`,
-	      					  `RF`.`F`,
-	      					  CONCAT('R', `RF`.`R1`,'-', 'F', `RF`.`F1`, '-', 'M', `RF`.`M1`) `RF`,
+	  `USERGROUPS` AS (SELECT `RFM`.`user_id`,
+	  						  `RFM`.`R`,
+	      					  `RFM`.`F`,
+	      					  CONCAT('R', `RFM`.`R1`,'-', 'F', `RFM`.`F1`, '-', 'M', `RFM`.`M1`) `RFM`,
 	      					  CASE  
-	            			  WHEN  CONCAT(`RF`.`R1`,`RF`.`F1`, `RF`.`M1`) 
+	            			  WHEN  CONCAT(`RFM`.`R1`,`RFM`.`F1`, `RFM`.`M1`) 
 	            			  	IN ("555", "455", "355", "345", "354", "445", "245", "255", "325", "335") 
 	            			  	THEN 'VIP' 
 	      
-	            			  WHEN  CONCAT(`RF`.`R1`,`RF`.`F1`, `RF`.`M1`) 
-	            			  	IN ("443", "441", "442", "444", "333", "341", "342", "343", "344", "321", "322", "323", "324", "334") 
+	            			  WHEN  CONCAT(`RFM`.`R1`,`RFM`.`F1`, `RFM`.`M1`) 
+	            			  	IN ("443", "441", "442", "444", "333", "341", "342", "343", "344", "321", "322", "323", "324", "334", "513", "512", "523", "524", "511", "514") 
 	            			  	THEN 'REGULAR' 
 
-	            		      WHEN CONCAT(`RF`.`R1`, `RF`.`F1`, `RF`.`M1`)
+	            		      WHEN CONCAT(`RFM`.`R1`, `RFM`.`F1`, `RFM`.`M1`)
 	            		      	IN ("221", "222", "223", "224", "115",  "225", "231", "232", "233", "234", "235", "241", "242", "243", "244", "251", "252", "253", "254", "311", "312", "313", "314", "315",  "124", "125", "211", "212")
 	            		      	THEN "HOPEFOOL"
 
-	            			  WHEN CONCAT(`RF`.`R1`,`RF`.`F1`, `RF`.`M1`) 
-	            			  	NOT IN ("111", "112", "113", "114", "121", "122", "123") 
+	            			  WHEN CONCAT(`RFM`.`R1`,`RFM`.`F1`, `RFM`.`M1`) 
+	            			    IN ("111", "112", "113", "114", "121", "122", "123", "134", "144") 
 	            			  	THEN 'LOST'
 
 	            			  ELSE 'undefined_group' 
 
 	      				      END as `G`
-	    			   FROM `RF`)
-  
+	    			   FROM `RFM`)
+
+-- consolidated table
 	  SELECT `u`.`G` `group`,
 	  		 `u`.`user_id` `user_id`, 
-	  		 `u`.`RF` `index`,
+	  		 `u`.`RFM` `index`,
+	  		 `t`.`count_by_rfm` `count_by_rfm`,
+	  		 -- `d`.`count_by_group` `count_by_group`,
 	  		 `R`.`days` `days`, 
 	  		 `F`.`cnt` `orders`,
-	  		 `m`.`sum_price` `sum`
+	  		 `m`.`sum_price` `sum`, 
+	  		 ROUND(`m`.`sum_price` / @overall_sum, 2) `% of turnover`
 
-	  FROM `USERGROUPS` `u` LEFT JOIN `M` `m` 
-	  ON `u`.`user_id` = `m`.`user_id` left join `R` ON `R`.`user_id` = `m`.`user_id`
-	  LEFT JOIN `F` ON `F`.`user_id` = `R`.`user_id`;
+	  FROM `USERGROUPS` `u` 
+	  LEFT JOIN `M` `m` ON `u`.`user_id` = `m`.`user_id` 
+	  LEFT JOIN `R` ON `R`.`user_id` = `m`.`user_id`
+	  LEFT JOIN `F` ON `F`.`user_id` = `R`.`user_id`
+	  LEFT JOIN (SELECT `u`.`user_id`, `u`.`RFM` `index`, COUNT(*) `count_by_rfm`
+	 				FROM `USERGROUPS` `u`
+	 				GROUP BY `index`) as t 
+	  		ON t.`user_id` = `F`.`user_id`
+
+	  		;
+
+-- count users by RFM
+	 -- SELECT `u`.`user_id`, `u`.`RFM` `u`.`RFM` `index`, COUNT(*) 
+	 -- FROM `USERGROUPS` `u`
+	 -- GROUP BY `index`;
+
+-- count users by groups where purchase value is greater than 0 
+	  -- SELECT `u`.`G` `group`,
+	  -- 		 COUNT(*) `cnt`
+
+	  -- FROM `USERGROUPS` `u` GROUP BY `group` 
+	  -- WITH ROLLUP;
+	 
+
+-- count all users where purchase value is greater than 0
+SELECT COUNT(DISTINCT user_id) cnt 
+FROM orders_20190822 
+WHERE price > 0;  
 
